ab7d74d26e1c0028b0f7ca14efd03934b30e99f7
SeethaRamaSai11249A069 <11249a069@kanchiuniv.ac.in>
2026-09-09 12:21:03 +0530

    Add files via upload

---
+++ ex 11a/Demo.java
@@ -0,0 +1,43 @@
+interface A {
+    void showA();
+}
+
+interface B extends A {
+    void showB();
+}
+
+interface C extends A {
+    void showC();
+}
+
+interface D extends B, C {
+    void showD();
+}
+
+class Demo implements D {
+
+    public void showA() {
+        System.out.println("Interface A");
+    }
+
+    public void showB() {
+        System.out.println("Interface B");
+    }
+
+    public void showC() {
+        System.out.println("Interface C");
+    }
+
+    public void showD() {
+        System.out.println("Interface D");
+    }
+
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+
+        obj.showA();
+        obj.showB();
+        obj.showC();
+        obj.showD();
+    }
+}
